0d3513dfad49eb3ed4fb2af9bb0f3958c5f4bdeb
Asiel Cabrera <cabrerasiel@gmail.com>
2023-05-18 16:19:01 -0400

    test 517 output

--- Sources/main.swift
+++ Sources/main.swift
@@ -76,4 +76,4 @@ public struct Core {
 let myInput = Core.getInput(name: "myInput")
 print("Valor de name: \(myInput ?? "N/A")")
 
-Core.setOutput(name: "myOutput", value: "test output")
+Core.setOutput(name: "myOutput", value: "Probando el output")
