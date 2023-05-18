b0e352af763f2429281ca175918dbece2d3b5ad9
Asiel Cabrera <cabrerasiel@gmail.com>
2023-05-18 09:43:47 -0400

    test 500

--- Sources/main.swift
+++ Sources/main.swift
@@ -64,21 +64,3 @@ public struct Core {
 
 let myInput = Core.getInput(name: "myInput")
 print("Valor de myInput: \(myInput ?? "N/A")")
-
-// Obtener una entrada booleana
-//let isDebug = Core.getBooleanInput(name: "debug")
-//print("¿Es modo de depuración? \(isDebug)")
-
-// Establecer el valor de una salida
-Core.setOutput(name: "myOutput", value: "Hello, world!")
-
-// Establecer un valor secreto y sensible
-//Core.setSecret(name: "mySecret", value: "s3cr3t")
-
-// let secret = Core.getSecret(name: "mySecret")
-
-// Imprimir un mensaje de depuración
-Core.debug(message: "Esto es un mensaje de depuración")
-
-// Indicar que la acción ha fallado
-// Core.setFailed(message: "Algo salió mal. La acción ha fallado.")
